a7b7777c2a60cb834445f9470c6ffcfa7ff9339f
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-06-17 12:46:08 +0100

    changed Javascript to use cookie

--- website/scripts.js
+++ website/scripts.js
@@ -1,48 +1,49 @@
-const myURL = "https://sj5qwca5eh.execute-api.eu-west-1.amazonaws.com/dev"
+const myURL = "https://sj5qwca5eh.execute-api.eu-west-1.amazonaws.com/dev";
 
 function hasVisitedBefore() {
-    return localStorage.getItem('visited') === 'true';
+  return document.cookie.includes("visited=true");
 }
 
 function setVisited() {
-    localStorage.setItem('visited', 'true');
+  document.cookie = "visited=true; path=/";
 }
 
 function fetchCount() {
-    return fetch(myURL)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch count');
-            }
-            return response.json();
-        });
+  return fetch(myURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch count');
+      }
+      return response.json();
+    });
 }
 
 function updateCount() {
-    if (!hasVisitedBefore()) {
-        fetch(myURL, { method: 'POST' }) // Perform a POST request to increment the count
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Failed to update count');
-            }
-            return fetchCount();
-        })
-            .then((count) => {
-                document.getElementById('counter').innerHTML = count;
-            })
-            .catch((error) => {
-                console.error(error);
-        });
-    }
+  if (!hasVisitedBefore()) {
+    fetch(myURL, { method: 'POST' }) // Perform a POST request to increment the count
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to update count');
+        }
+        return fetchCount();
+      })
+      .then((count) => {
+        document.getElementById('counter').innerHTML = count;
+        setVisited();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
 
 function getCount() {
-    fetchCount()
+  fetchCount()
     .then((count) => {
-    document.getElementById('counter').innerHTML = count;
+      document.getElementById('counter').innerHTML = count;
     })
     .catch((error) => {
-    console.error(error);
+      console.error(error);
     });
 }
 
