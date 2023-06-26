d7b9c782e4312185f4f5c244ad0d8b631edb4349
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-06-26 23:26:50 +0100

    added DNSSEC

--- route53.tf
+++ route53.tf
@@ -12,3 +12,54 @@ resource "aws_route53_record" "resume_website" {
     evaluate_target_health = false
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "resume_website" {
+  provider                 = aws.us-east-1
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 7
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+          "kms:Verify",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service",
+        Resource = "*"
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+# Follow the AWS "Disabling DNSSEC signing" guide before deleting  
+resource "aws_route53_key_signing_key" "resume_website" {
+  hosted_zone_id             = aws_route53_zone.resume_website.id
+  key_management_service_arn = aws_kms_key.resume_website.arn
+  name                       = "example"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "resume_website" {
+  depends_on = [
+    aws_route53_key_signing_key.resume_website
+  ]
+  hosted_zone_id = aws_route53_key_signing_key.resume_website.hosted_zone_id
+}
