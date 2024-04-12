ac0b72e4ee2c2191eff224a9d4aeec2b49c4c96f
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2024-04-12 18:52:34 +0100

    updating email script

--- website/email-script.js
+++ website/email-script.js
@@ -1,8 +1,8 @@
 // Call this function to reveal the email address
 function revealEmail() {
-  var emailElement = document.getElementById("emailLink");
-  var encodedEmail = "ZGF2aWQudC5vY29ubm9yQG91dGxvb2suaWU="; // Base64-encoded email address
-  var decodedEmail = atob(encodedEmail);
+  const emailElement = document.getElementById("emailLink");
+  const encodedEmail = "ZGF2aWQudC5vY29ubm9yQG91dGxvb2suaWU="; // Base64-encoded email address
+  const decodedEmail = atob(encodedEmail);
   emailElement.href = "mailto:" + decodedEmail;
   emailElement.textContent = decodedEmail;
 }
